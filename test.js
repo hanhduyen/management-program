673d581bcc19545025ccffe4c4666bc1c74abddb
hanhduyen <sarah051608@gmail.com>
2024-04-08 14:20:07 +0700

    test

---
+++ test.js
@@ -0,0 +1,18 @@
+const inputBox=document.getElementById("input-box");
+const titleBox=document.getElementById("title-input");
+const contentBox=document.getElementById("content");
+function addTask() {
+    const inputBox=document.getElementById("input-box");
+    const titleBox=document.getElementById("title-input");
+    const contentBox=document.getElementById("content");
+    
+    let li = document.createElement("li") 
+}
+let text =" ";
+const  newTask = {
+    category: marketing,
+    title: write,
+    status:todo,
+};
+const newTask2=document.getElementById("card").innerHTML;
+newTask2=text;
